23ba4d90869b5761de3418b1cfa70878b800440d
Runbier Sabhlok <superrunrocks@gmail.com>
2022-11-05 15:59:49 -0400

    Just a test to make sure version control works 2.

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception{
         System.out.println("USACO is great, but its hard. ");
+        System.out.println("Does this work?");
     }
 }
