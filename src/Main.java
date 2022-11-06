9cedb57930741e352087d6a772d21f6bad616fd6
Runbier Sabhlok <superrunrocks@gmail.com>
2022-11-05 21:50:26 -0400

    Problem: Measuring Traffic Status: Not finished

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
 import java.util.*;
 import java.io.*;
 
+// Measuring Traffic
 public class Main {
     public static void main(String[] args) throws Exception{
-        System.out.println("USACO is great, but its hard. ");
-        System.out.println("Does this work?");
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        String[] segmentType = new String[N];
+        int[] lowerBounds = new int[N];
+        int[] upperBounds = new int[N];
+
+        for(int i = 0; i < N; i++){
+            segmentType[i] = scanner.next();
+            lowerBounds[i] = scanner.nextInt();
+            upperBounds[i] = scanner.nextInt();
+
+
+            if((segmentType[i].equals("on") && segmentType[i+1].equals("none")) || (segmentType[i].equals("on") && segmentType[i+1].equals("on"))){
+                int newLowerBounds = lowerBounds[i] + lowerBounds[i + 1];
+                int newUpperBounds = upperBounds[i] + upperBounds[i + 1];
+                System.out.println("New Lower Bounds: " + newLowerBounds);
+                System.out.println("New Upper Bounds: " + newUpperBounds);
+            }
+
+        }
+
     }
 }
