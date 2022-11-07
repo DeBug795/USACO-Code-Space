4ae5aaf7a247fb4e4a175134c9ffc6384b78b5e0
Runbier Sabhlok <superrunrocks@gmail.com>
2022-11-06 20:11:49 -0500

    Problem: Mixing Milk USACO Status: Not finished

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,35 @@
 import java.util.*;
-import java.io.*;
 
-// Measuring Traffic
+// Mixing Milk
 public class Main {
+
+    static int pour(int bucketACurrent, int bucketBCapacity, int bucketBCurrent){
+        /*if(bucketACurrent <= bucketBCapacity && (bucketBCurrent + bucketACurrent <= bucketBCapacity)){
+            bucketBCurrent = bucketBCurrent + bucketACurrent;
+            return bucketBCurrent;
+        }*/
+
+        bucketBCurrent = bucketBCurrent + bucketACurrent;
+        return bucketBCurrent;
+        /*bucketACurrent = bucketACurrent + bucketBCurrent - bucketBCapacity;
+        return bucketBCapacity;*/
+    }
+
     public static void main(String[] args) throws Exception{
         Scanner scanner = new Scanner(System.in);
 
-        int N = scanner.nextInt();
-        String[] segmentType = new String[N];
-        int[] lowerBounds = new int[N];
-        int[] upperBounds = new int[N];
+        // bucket capacity [element 0] bucket current [element 1]
+        int[] bucket1 = {scanner.nextInt(), scanner.nextInt()};
+        int[] bucket2 = {scanner.nextInt(), scanner.nextInt()};
+        int[] bucket3 = {scanner.nextInt(), scanner.nextInt()};
 
-        for(int i = 0; i < N; i++){
-            segmentType[i] = scanner.next();
-            lowerBounds[i] = scanner.nextInt();
-            upperBounds[i] = scanner.nextInt();
-
-
-            if((segmentType[i].equals("on") && segmentType[i+1].equals("none")) || (segmentType[i].equals("on") && segmentType[i+1].equals("on"))){
-                int newLowerBounds = lowerBounds[i] + lowerBounds[i + 1];
-                int newUpperBounds = upperBounds[i] + upperBounds[i + 1];
-                System.out.println("New Lower Bounds: " + newLowerBounds);
-                System.out.println("New Upper Bounds: " + newUpperBounds);
-            }
-
+        for(int i = 0; i <= 100; i++){
+            int newPourA = pour(bucket1[1], bucket2[0], bucket2[1]);
+            int newPourB = pour(bucket2[1], bucket3[0], bucket3[1]);
+            int newPourC = pour(bucket3[1], bucket1[0], bucket1[1]);
+            System.out.println(newPourA);
+            System.out.println(newPourB);
+            System.out.println(newPourC);
         }
-
     }
 }
