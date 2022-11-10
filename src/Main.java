9d189c6e690055a94a407a661c4d9b8c42f1ac3b
Runbier Sabhlok <superrunrocks@gmail.com>
2022-11-09 19:06:59 -0500

    Problem: Milk Measurement USACO Status: Almost done

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,58 @@
 import java.util.*;
 
-// Distinct Numbers
-public class Main {
-    public static void main(String[] args){
+// Milk Measurement
+class Main {
+
+    public static int amountMilk(String dayNameMilkLINE, int initialMilk) {
+        char[] charInputArr = dayNameMilkLINE.toCharArray();
+
+        int arrLength = charInputArr.length;
+
+        System.out.println(arrLength);
+
+        int amountMilkGainedLost = charInputArr[charInputArr.length - 1];
+        char operation = (charInputArr[charInputArr.length - 2]);
+
+        if (operation == '+') {
+            initialMilk += amountMilkGainedLost;
+        } else {
+            initialMilk -= amountMilkGainedLost;
+        }
+
+        return initialMilk;
+    }
+
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Set<Integer> set = new HashSet<> ();
         int N = scanner.nextInt();
-        int[] input = new int[N];
+        int initialMilk = 7;
 
-        for(int i = 0; i < N; i++){
-            input[i] = scanner.nextInt();
-            set.add(input[i]);
+        String[] daysNamesMilk = new String[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            daysNamesMilk[i] = scanner.nextLine();
         }
-        System.out.println(set.size());
+
+        Arrays.sort(daysNamesMilk);
+
+        System.out.println("What does the input array contain");
+        for (int y = 0; y <= N; y++) {
+            System.out.println(daysNamesMilk[y]);
+        }
+
+        int counter = 0;
+
+        for (int w = 1; w <= N-1; w++) {
+            int[] milk = new int[N];
+            milk[w] = amountMilk(daysNamesMilk[w], initialMilk);
+
+            if (milk[w] > initialMilk) {
+                counter++;
+            }
+        }
+
+        System.out.println(counter + 1);
+
     }
 }
