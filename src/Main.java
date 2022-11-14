fe954904e728cb4caaa29d60d9f58c0fad3ffbf4
Runbier Sabhlok <superrunrocks@gmail.com>
2022-11-14 18:39:15 -0500

    Problem: Weird Algorithm - CSES Status: done (fix: just use variable LONG).

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
+        long N = scanner.nextLong();
         System.out.print(N + " ");
 
         while(N != 1){
