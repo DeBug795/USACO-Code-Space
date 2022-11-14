6efc9ece5ac6e963510da11e2f297027481746b3
Runbier Sabhlok <superrunrocks@gmail.com>
2022-11-13 19:14:27 -0500

    Problem: Weird Algorithm - CSES Status: done (time limit exceeded)

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,22 @@
 import java.io.*;
 import java.util.*;
 
-//  The Cow Signal
+//  Weird Algorithm CSES
 class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(new File("cowsignal.in"));
-        PrintWriter pw = new PrintWriter(new FileWriter("cowsignal.out"));
-
-        int M = scanner.nextInt();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
-        int K = scanner.nextInt();
+        System.out.print(N + " ");
 
-        for(int i = 0; i < M; i++){
-            String pic = scanner.next();
-            String extended = "";
-
-            for (char c: pic.toCharArray()) {
-                for(int j = 0; j < K; j++){
-                    extended += c;
-                }
-            }
-
-            //code
-            for(int j = 0; j < K; j++){
-                pw.println(extended);
+        while(N != 1){
+            if(N % 2 != 0){
+                N = N * 3 + 1;
+                System.out.print(N + " ");
+            } else{
+                N = N / 2;
+                System.out.print(N + " ");
             }
         }
-
-        pw.close();
 
     }
 }
