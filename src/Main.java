7731ad72ad4b8dbf89927202c6d3b62ea05f46e2
Runbier Sabhlok <superrunrocks@gmail.com>
2022-11-14 20:36:24 -0500

    Problem: Missing Numbers - CSES Status: done (doesn't pass all test cases).

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,23 @@
-import java.io.*;
 import java.util.*;
 
-//  Weird Algorithm CSES
+//  Missing Number CSES
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        long N = scanner.nextLong();
-        System.out.print(N + " ");
+        int N = scanner.nextInt();
 
-        while(N != 1){
-            if(N % 2 != 0){
-                N = N * 3 + 1;
-                System.out.print(N + " ");
-            } else{
-                N = N / 2;
-                System.out.print(N + " ");
-            }
+        int[] nums = new int[N-1];
+        for(int i = 0; i < N - 1; i++){
+            nums[i] = scanner.nextInt();
         }
 
+        Arrays.sort(nums);
+
+        for(int i = 1; i < N - 1; i++){
+            if(nums[i + 1] != nums[i] + 1){
+                System.out.println(nums[i] + 1);
+                break;
+            }
+        }
     }
 }
