5b8920726dd209ba12790e2df0310a126a30b9d8
Runbier Sabhlok <superrunrocks@gmail.com>
2022-11-16 20:17:05 -0500

    Problem: Marathon 2014 - USACO Status: not done

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,30 @@
+import java.io.*;
 import java.util.*;
 
-//  Missing Number CSES
+//  Marathon 2014 USACO
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
 
-        int[] nums = new int[N-1];
-        for(int i = 0; i < N - 1; i++){
-            nums[i] = scanner.nextInt();
+        int N = scanner.nextInt();
+        int[] cowPosition = new int[N];
+        int[] cowSpeed = new int[N];
+
+        for(int i = 0; i < N; i++){
+            cowPosition[i] = scanner.nextInt();
+            cowSpeed[i] = scanner.nextInt();
         }
 
-        Arrays.sort(nums);
-
-        for(int i = 1; i < N - 1; i++){
-            if(nums[i + 1] != nums[i] + 1){
-                System.out.println(nums[i] + 1);
-                break;
+        int groupCounter = 0;
+        //Changing the value of i changes the output.
+        for(int i = 0; i < N - 1; i++){
+            if(cowSpeed[i] < cowSpeed[i + 1]){
+                groupCounter++;
+            }else{
+                groupCounter++;
             }
         }
+        System.out.println(groupCounter);
+
     }
 }
