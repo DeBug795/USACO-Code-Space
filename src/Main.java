99cd44c65a53a85cac85331fa78a2ce5a3c28017
Runbier Sabhlok <superrunrocks@gmail.com>
2022-11-16 20:18:40 -0500

    Problem: Do You Know Your ABCs? 2020 USACO Status: Finished. Passed all test cases

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,24 @@
-import java.io.*;
 import java.util.*;
 
-//  Marathon 2014 USACO
+//  Do You Know Your ABCs? 2020 USACO
 class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        int[] input = new int[7];
 
-        int N = scanner.nextInt();
-        int[] cowPosition = new int[N];
-        int[] cowSpeed = new int[N];
-
-        for(int i = 0; i < N; i++){
-            cowPosition[i] = scanner.nextInt();
-            cowSpeed[i] = scanner.nextInt();
+        for(int i = 0; i < 7; i++) {
+            input[i] = scanner.nextInt();
         }
 
-        int groupCounter = 0;
-        //Changing the value of i changes the output.
-        for(int i = 0; i < N - 1; i++){
-            if(cowSpeed[i] < cowSpeed[i + 1]){
-                groupCounter++;
-            }else{
-                groupCounter++;
-            }
+        Arrays.sort(input);
+        int c = input[6] - (input[0] + input[1]);
+
+        if(input[0] + input[1] + input[2] == input[6]) {
+            System.out.println(input[0] + " " + input[1] + " " + c);
         }
-        System.out.println(groupCounter);
+        else {
+            System.out.println(input[0] + " " + input[1] + " " + c);
+        }
 
     }
 }
